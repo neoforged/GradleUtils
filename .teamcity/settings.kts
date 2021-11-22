dbf0d1a96df35b0436e30690c400d2cd4a01295d
OrionDevelopment <OrionDevelopment@TeamCity>
2021-11-22 19:26:53 +0000

    TeamCity change in 'MinecraftForge / GradleUtils' project: 'https://github.com/MinecraftForge/GradleUtils.git' VCS root was created

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,5 +1,6 @@
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
 import jetbrains.buildServer.configs.kotlin.v2019_2.projectFeatures.githubIssues
+import jetbrains.buildServer.configs.kotlin.v2019_2.vcs.GitVcsRoot
 
 /*
 The settings script is an entry point for defining a TeamCity
@@ -26,6 +27,8 @@ To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
 version = "2021.2"
 
 project {
+
+    vcsRoot(HttpsGithubComMinecraftForgeGradleUtilsGit)
 
     buildType(Build)
     buildType(PullRequests_1)
@@ -54,4 +57,10 @@ object PullRequests_1 : BuildType({
     id("PullRequests")
     name = "Pull Requests"
     description = "Builds pull requests for the project"
+})
+
+object HttpsGithubComMinecraftForgeGradleUtilsGit : GitVcsRoot({
+    name = "https://github.com/MinecraftForge/GradleUtils.git"
+    url = "https://github.com/MinecraftForge/GradleUtils.git"
+    branch = "refs/heads/main"
 })
