b2faf5de0e366d00e262b2b82f1e35ff9299adf8
sciwhiz12 <arnoldnunag12@gmail.com>
2023-09-10 02:03:54 +0800

    Migrate changelog extension to Groovy
    This brings this class in line with the other classes in the package.

---
+++ src/main/groovy/net/minecraftforge/gradleutils/ChangelogGenerationExtension.groovy
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) NeoForged and contributors
+ * SPDX-License-Identifier: LGPL-2.1-only
+ */
+
+package net.minecraftforge.gradleutils
+
+import org.gradle.api.Project
+import org.gradle.api.publish.maven.MavenPublication
+
+import javax.inject.Inject
+
+class ChangelogGenerationExtension {
+    private final Project project
+    private boolean registerAllPublications = true
+
+    @Inject
+    ChangelogGenerationExtension(Project project) {
+        this.project = project
+    }
+
+    void fromMergeBase() {
+        ChangelogUtils.setupChangelogGeneration(project)
+        project.afterEvaluate(this::afterEvaluate)
+    }
+
+    void fromTag(final String tag) {
+        ChangelogUtils.setupChangelogGenerationFromTag(project, tag)
+        project.afterEvaluate(this::afterEvaluate)
+    }
+
+    void fromCommit(final String commit) {
+        ChangelogUtils.setupChangelogGenerationFromCommit(project, commit)
+        project.afterEvaluate(this::afterEvaluate)
+    }
+
+    void disableAutomaticPublicationRegistration() {
+        this.registerAllPublications = false
+    }
+
+    void publish(final MavenPublication mavenPublication) {
+        ChangelogUtils.setupChangelogGenerationForPublishing(project, mavenPublication)
+    }
+
+    private void afterEvaluate(final Project project) {
+        if (registerAllPublications)
+            ChangelogUtils.setupChangelogGenerationOnAllPublishTasks(project);
+    }
+}
