1aae04f51a15799b48762b1b31c22206b2e5ca4f
Technici4n <13494793+Technici4n@users.noreply.github.com>
2023-10-30 15:25:11 +0100

    Don't cache GenerateChangelogTask

--- src/main/groovy/net/neoforged/gradleutils/tasks/GenerateChangelogTask.java
+++ src/main/groovy/net/neoforged/gradleutils/tasks/GenerateChangelogTask.java
@@ -32,6 +32,7 @@ import org.gradle.api.tasks.Input;
 import org.gradle.api.tasks.Nested;
 import org.gradle.api.tasks.OutputFile;
 import org.gradle.api.tasks.TaskAction;
+import org.gradle.work.DisableCachingByDefault;
 
 import javax.inject.Inject;
 import java.io.File;
@@ -39,6 +40,7 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 
+@DisableCachingByDefault(because = "The Git state is not an Input therefore we can't cache this task for now")
 public abstract class GenerateChangelogTask extends DefaultTask {
 
     public GenerateChangelogTask() {
